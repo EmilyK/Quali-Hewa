a03c202a0ee312c88e1d7218e2a1aeb0c8d69287
Ems <ekar45@gmail.com>
2014-05-13 19:48:22 +0300

    its coming to 8...tired

--- hewa/migrations/0001_initial.py
+++ hewa/migrations/0001_initial.py
@@ -8,15 +8,31 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Adding model 'AirQualityReading'
+        db.create_table(u'hewa_airqualityreading', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('carbonmonoxide_sensor_reading', self.gf('django.db.models.fields.DecimalField')(default=0.0, max_digits=10, decimal_places=3)),
+            ('nitrogen_sensor_reading', self.gf('django.db.models.fields.DecimalField')(default=0.0, max_digits=10, decimal_places=3)),
+            ('gas_sensor_reading', self.gf('django.db.models.fields.DecimalField')(default=0.0, max_digits=10, decimal_places=3)),
+        ))
+        db.send_create_signal(u'hewa', ['AirQualityReading'])
+
         # Adding model 'Analyser'
         db.create_table(u'hewa_analyser', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('analyser_id', self.gf('django.db.models.fields.IntegerField')(max_length=100)),
-            ('sensor_type', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('sensor_reading', self.gf('django.db.models.fields.IntegerField')(max_length=500)),
-            ('reading_time', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('carbonmonoxide_sensor_present', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('nitrogen_sensor_present', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('gas_sensor_present', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
         db.send_create_signal(u'hewa', ['Analyser'])
+
+        # Adding M2M table for field readings on 'Analyser'
+        db.create_table(u'hewa_analyser_readings', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('analyser', models.ForeignKey(orm[u'hewa.analyser'], null=False)),
+            ('airqualityreading', models.ForeignKey(orm[u'hewa.airqualityreading'], null=False))
+        ))
+        db.create_unique(u'hewa_analyser_readings', ['analyser_id', 'airqualityreading_id'])
 
         # Adding model 'Station'
         db.create_table(u'hewa_station', (
@@ -30,21 +46,34 @@ class Migration(SchemaMigration):
 
 
     def backwards(self, orm):
+        # Deleting model 'AirQualityReading'
+        db.delete_table(u'hewa_airqualityreading')
+
         # Deleting model 'Analyser'
         db.delete_table(u'hewa_analyser')
+
+        # Removing M2M table for field readings on 'Analyser'
+        db.delete_table('hewa_analyser_readings')
 
         # Deleting model 'Station'
         db.delete_table(u'hewa_station')
 
 
     models = {
+        u'hewa.airqualityreading': {
+            'Meta': {'object_name': 'AirQualityReading'},
+            'carbonmonoxide_sensor_reading': ('django.db.models.fields.DecimalField', [], {'default': '0.0', 'max_digits': '10', 'decimal_places': '3'}),
+            'gas_sensor_reading': ('django.db.models.fields.DecimalField', [], {'default': '0.0', 'max_digits': '10', 'decimal_places': '3'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'nitrogen_sensor_reading': ('django.db.models.fields.DecimalField', [], {'default': '0.0', 'max_digits': '10', 'decimal_places': '3'})
+        },
         u'hewa.analyser': {
             'Meta': {'object_name': 'Analyser'},
-            'analyser_id': ('django.db.models.fields.IntegerField', [], {'max_length': '100'}),
+            'carbonmonoxide_sensor_present': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'gas_sensor_present': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'reading_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'sensor_reading': ('django.db.models.fields.IntegerField', [], {'max_length': '500'}),
-            'sensor_type': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+            'nitrogen_sensor_present': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'readings': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['hewa.AirQualityReading']", 'symmetrical': 'False'})
         },
         u'hewa.station': {
             'Meta': {'object_name': 'Station'},
