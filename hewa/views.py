d398ba7638e1f9b747af6deeaff3e37dd6515ede
Ems <ekar45@gmail.com>
2014-06-05 18:36:04 +0300

    detail view

--- hewa/views.py
+++ hewa/views.py
@@ -2,35 +2,59 @@ from django.shortcuts import render, RequestContext
 
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django_tables2 import RequestConfig
 from hewa.tables import StationTable
 from hewa.models import Station, Analyser
+from .forms import StationForm
+from django.views.generic.detail import DetailView
 
 
 def index(request):
-    table = StationTable(Station.objects.all())
-    RequestConfig(request, paginate={"per_page": 25}).configure(table)#Pulls values from request.GET and updates the table accordingly
-    # analysers = [s.analyser for s in Station.objects.all()] # Option 1
-    analysers = Analyser.objects.exclude(station=None) # Option 2
+    analysers = Analyser.objects.exclude(station=None)
+    form = StationForm()
 
-    # readings =>> [(station_name, co, no, lpg), ]
-    readings = []
-    for analyser in analysers:
-    	if analyser.readings.exists():
-	    	latest_reading = analyser.readings.latest('created_at')
-	    	readings.append(
-	    		(analyser.station_set.latest('station_name'),
-    		    		latest_reading.carbonmonoxide_sensor_reading,
-    		    		latest_reading.nitrogendioxide_sensor_reading,
-    		    		latest_reading.lpg_gas_sensor_reading,
-    		    		latest_reading.created_at)
-    		)
+    if request.method == 'POST':
+        form = StationForm(request.POST)
+        station_name = form.data['autocomplete']
 
-    return render(request, "hewa/index.html", 
-    	{"stations": Station.objects.all(),
-    	 'readings': readings})
-	# return render_to_response('hewa/index.html', {}, RequestContext(request))
+        if station_name:
+            station = Station.objects.filter(station_name__icontains=station_name)
+            if station.exists():
+                station = station[0]
+                return redirect('station-detail', pk=station.pk)
+            else:
+                return redirect('index')
+        else:
+            return redirect('index') # user hasn't typed anything in the search box
+    else:
+        form = StationForm()
+        readings = []
+        for analyser in analysers:
+            if analyser.readings.exists():
+                latest_reading = analyser.readings.latest('created_at')
+                readings.append((analyser.station_set.latest('station_name'),
+                    latest_reading.carbonmonoxide_sensor_reading,
+                    latest_reading.nitrogendioxide_sensor_reading,
+                    latest_reading.lpg_gas_sensor_reading,
+                    latest_reading.created_at))
+        return render_to_response('hewa/index.html', {'form': form, 'stations': Station.objects.all(), 
+            'readings': readings}, RequestContext(request))
+
+
+class StationDetailView(DetailView):
+
+    model = Station
+
+    def get_context_data(self, **kwargs):
+        context = super(StationDetailView, self).get_context_data(**kwargs)
+        # you can override the contxt dict values
+        context['form'] = StationForm()
+        station = context['object']
+        readings = station.analyser.readings.all()
+        context['readings'] = readings
+        return context
+
 
 def station_json(request):
 	# http://stackoverflow.com/questions/20890955/mapbox-show-tooltips-by-default-without-having-to-click-a-marker
