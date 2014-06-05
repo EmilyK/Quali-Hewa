f1c7f28f921ac2471889588c5068ac7f2e66a3c8
Ems <ekar45@gmail.com>
2014-06-05 18:37:16 +0300

    search logic

---
+++ hewa/lookups.py
@@ -0,0 +1,18 @@
+from selectable.base import ModelLookup
+from selectable.registry import registry
+from .models import Station, Analyser, AirQualityReading
+
+
+class StationLookup(ModelLookup):
+
+	model = Station
+	search_fields = (
+		'station_name__icontains',)
+		#'lat__icontains',
+		#'lon__icontains')
+
+
+# class AnalyserLookup(ModelLookup):
+# 	model = Analyser
+
+registry.register(StationLookup)
