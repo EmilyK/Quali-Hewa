40f0a84cea9cfcbf7092cba8dc73d24df7a76519
Ems <ekar45@gmail.com>
2014-06-10 19:03:27 +0300

    changed the chart to take both monthly and weekly readings

--- hewa/static/js/chart.js
+++ hewa/static/js/chart.js
@@ -1,3 +1,4 @@
+/* function to generate chart */
 function generateChart(url, title){
 
     $.getJSON(url, function(data){
@@ -38,12 +39,15 @@ function generateChart(url, title){
 
 }
 
+
+
+
 $(function(){
+    /* this function handles options chosen by user */
     $('#selectBox').change(function(){
         var selectedBox = $('#selectBox')[0];
         var selectedValue = selectBox.options[selectBox.selectedIndex].value;
         if (selectedValue === 'monthly'){
-            console.log('starting monthly');
             generateChart('/chart-data/monthly', 'Monthly Total readings');
         }else{
             generateChart('/chart-data', 'Weekly Total readings');
@@ -53,41 +57,6 @@ $(function(){
 
 
 $(document).ready(function(){
-    $.getJSON('/chart-data', function(data){
-       
-        $('#container').highcharts({
-            title: {
-                text: 'Weekly total Readings',
-                x: -20 //center
-            },
-            subtitle: {
-                text: 'Raspberry pi readings',
-                x: -20
-            },
-            xAxis: {
-                categories: data['dates']//['Sun', 'Sat', 'Fri', 'Thur', 'Wed', 'Tue', 'Mon']
-            },
-            yAxis: {
-                title: {
-                    text: 'Readings'
-                },
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                valueSuffix: 'units'
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle',
-                borderWidth: 0
-            },
-            series: data['payload']
-        });
-    });
-
+    /* this is the default */
+    generateChart('/chart-data', 'Weekly Total readings');
 });
