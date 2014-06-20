14edfc8c1793108f72c17f42cab212599c2a62e8
Ems <ekar45@gmail.com>
2014-06-20 18:20:21 +0300

    generic chart for front page

--- hewa/static/js/chart.js
+++ hewa/static/js/chart.js
@@ -1,53 +1,4 @@
-/* function to generate chart */
-function generateChart(url, title, tooltip){
-
-    $.getJSON(url, function(data){
-        $('#container').highcharts({
-            title: {
-                text: title,
-                x: -20 //center
-            },
-            subtitle: {
-                text: 'QualiHewa Analyser Readings',
-                x: -20
-            },
-            xAxis: {
-                categories: data['dates']
-            },
-            yAxis: {
-                title: {
-                    text: 'Readings(parts per million)'
-                },
-                plotLines: [{
-                color: '#FFFF00',
-                width: 2,
-                value: 30,
-                label: {text: "lpg gas limit at 30pmm"}
-                }, {
-                color: 'red',
-                width: 2,
-                value: 6,
-                label: {text: "carbon monoxide limit at 6pmm"} 
-                }]
-            },
-            tooltip: {
-                valueSuffix: 'ppm'
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle',
-                borderWidth: 0
-            },
-            series: data['payload']
-        });
-    });
-
-}
-
-
-
-
+/*for home page only*/
 $(function(){
     /* this function handles options chosen by user */
     $('#selectBox').change(function(){
@@ -62,7 +13,6 @@ $(function(){
         }
     })
 });
-
 
 $(document).ready(function(){
     /* this is the default */
