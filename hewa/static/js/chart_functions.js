b761e02de5769150edd4fbb635cc10338692c435
Ems <ekar45@gmail.com>
2014-06-20 18:16:30 +0300

    refactored chart functions

---
+++ hewa/static/js/chart_functions.js
@@ -0,0 +1,47 @@
+/* function to generate chart */
+function generateChart(url, title, tooltip){
+    console.log(url);
+    $.getJSON(url, function(data){
+        console.log(data);
+        $('#container').highcharts({
+            title: {
+                text: title,
+                x: -20 //center
+            },
+            subtitle: {
+                text: 'QualiHewa Analyser Readings',
+                x: -20
+            },
+            xAxis: {
+                categories: data['dates']
+            },
+            yAxis: {
+                title: {
+                    text: 'Readings(parts per million)'
+                },
+                plotLines: [{
+                color: '#FFFF00',
+                width: 2,
+                value: 30,
+                label: {text: "lpg gas limit at 30pmm"}
+                }, {
+                color: 'red',
+                width: 2,
+                value: 6,
+                label: {text: "carbon monoxide limit at 6pmm"} 
+                }]
+            },
+            tooltip: {
+                valueSuffix: 'ppm'
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: data['payload']
+        });
+    });
+
+}
