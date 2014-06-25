4658de3d5f5f2d527cd0b36eb83195052fa70ffa
Ems <ekar45@gmail.com>
2014-06-25 19:18:58 +0300

    deprecated use of an API, add upload url

--- quali/urls.py
+++ quali/urls.py
@@ -6,14 +6,14 @@ from tastypie.api import Api
 admin.autodiscover()
 
 
-v1_api = Api(api_name= 'v1')
-v1_api.register(StationResource())
-v1_api.register(AnalyserResource())
-v1_api.register(AirQualityReadingResource())
+# v1_api = Api(api_name= 'v1')
+# v1_api.register(StationResource())
+# v1_api.register(AnalyserResource())
+# v1_api.register(AirQualityReadingResource())
 
 urlpatterns = patterns('',
- 	url(r'^$',views.index, name = 'index'),
-    url(r'^about/$',views.about, name = 'about'),
+ 	url(r'^$',views.home, name = 'index'),
+    url(r'^dashboard/$',views.dashboard, name = 'dashboard'),
     url(r'^stations/$',views.station_list, name = 'stations'),
     url(r'^stations/(?P<pk>\d+)/$', views.station_detail, name='station-detail'),
     url(r'^chart-data/$',views.chart_json, name = 'chart-data-all'),
@@ -26,7 +26,8 @@ urlpatterns = patterns('',
     url(r'^chart-data/stations/(?P<pk>\d+)/daily/$',views.chart_json_station_d, name = 'chart-data-station-daily'),
     url(r'^chart-data/$',views.chart_json, name = 'chart-data'),
     url(r'^admin/', include(admin.site.urls)),
- 	(r'^api/', include(v1_api.urls)),
+    url(r'^upload/$', views.upload),
+ 	# (r'^api/', include(v1_api.urls)),
  	(r'^selectable/', include('selectable.urls')),
  	url(r'^excel/$', views.export, name = 'generate-excel'),
     url(r'^export-pdf/$', views.export_pdf, name = 'generate-pdf'),
