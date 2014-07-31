7145663cba6ddd2b9ab6624e84132aaedb5777e6
Phillip Karamagi <pkaramagi1@gmail.com>
2014-07-31 11:46:15 +0300

    Comments
    Added comments, to models.py and tables.py

--- hewa/models.py
+++ hewa/models.py
@@ -3,14 +3,21 @@ import datetime
 
 
 class AirQualityReading(models.Model):
-    """
-    AirQualityReading is a model that keeps record of all the readings captured by the
-    different sensors.
-    """
+    
+    #AirQualityReading is a model that keeps record of all the readings captured by the
+    #different sensors.
+    
+	#carbonmonoxide_sensor_reading takes maximum of 10 digits, at 3 decimal places stores Carbonmonoxide reading 		
     carbonmonoxide_sensor_reading = models.DecimalField(max_digits=10, default=0.00, decimal_places=3)
-    nitrogendioxide_sensor_reading = models.DecimalField(max_digits=10, default=0.00, decimal_places=3)
-    lpg_gas_sensor_reading = models.DecimalField(max_digits=10, default=0.00, decimal_places=3)
-    created_at = models.DateTimeField(
+
+    #nitrogendioxide_sensor_reading stores/captures nitrogendioxide
+	nitrogendioxide_sensor_reading = models.DecimalField(max_digits=10, default=0.00, decimal_places=3)
+
+    #lpg_gas_sensor_reading stores lpg gas reading
+	lpg_gas_sensor_reading = models.DecimalField(max_digits=10, default=0.00, decimal_places=3)
+    
+	#created_at is a time stamp for airquality reading.
+	created_at = models.DateTimeField(
         auto_now=True,
         auto_now_add=True,
         null=True, 
@@ -18,44 +25,32 @@ class AirQualityReading(models.Model):
 
 
 class Analyser(models.Model):    
-    """
-    How to work with the API; and using the Analyser,
-    Django provides a wonderful API
-    Note: this is used only with `python manage.py shell` or 
-    `python manage.py shell_plus`
-
-    CRUD operations
-
-    Create:
-    >>> Analyser.object.create() # creates an empty record
-
-    We can store this in a variable
-    >>> analyser = Analyser.objects.create() # our defaults be kawa, you know.
-    >>> analyser.save() # use the `save()` method to save the analyser
-
-    Read:
-    >>> Analyser.objects.all() # fetches all analysers
-
-    You can filter or pick a particular set of analysers too
-    >>> Analyser.objects.filter(id=1) # e.g. by id or primary key (kisumulozo)
-    >>> Analyser.objects.filter(pk=1)
-
-    Or search by fields or columns
-    >>> Analyser.objects.filter(carbonmonoxide_sensor_present=False)
-    """
-    identifier = models.CharField(unique=True, null=True, max_length=140)
-    carbonmonoxide_sensor_present = models.BooleanField(default=False)
-    nitrogendioxide_sensor_present = models.BooleanField(default=False)
-    lpg_gas_sensor_present = models.BooleanField(default=False)
-    readings = models.ManyToManyField(AirQualityReading, blank=True)
+  
+    #indentifier is a unique ID of analyser
+	identifier = models.CharField(unique=True, null=True, max_length=140)
+	
+    #carbonmonoxide_sensor_present , bolean ,shows if CO sensor is present
+	carbonmonoxide_sensor_present = models.BooleanField(default=False)
+    
+	#nitrogendioxide_sensor_present , bolean, shows if NO2 sensor is present
+	nitrogendioxide_sensor_present = models.BooleanField(default=False)
+    
+	#lpg_gas_sensor_present, bolean ,shows if lpg gas is present
+	lpg_gas_sensor_present = models.BooleanField(default=False)
+    
+	#reading stores airqualirtreading
+	readings = models.ManyToManyField(AirQualityReading, blank=True)
+	
+	#time at which analyser was registered 
     registered_at = models.DateTimeField(auto_now=True, default=datetime.datetime.today())
 
     #sets text reference for each record
     def __unicode__(self):
         if self.station_set.exists():
-            # does this analyser belong to a station?
+            # check if analyser belongs to a station
             return "{}".format(self.station_set.all()[0].station_name)
         else:
+			#message returned if analyser doesnot belong to any station
             return "Not in station"
         # Note; use of new string format method above
         # same as `return "%s" % self.variable`
@@ -63,15 +58,26 @@ class Analyser(models.Model):
 # Association
 # An analyser has_many Readings
 
+#class station represents each station where analysers are installed
 class Station(models.Model):
+
+	#station name - name of station	
     station_name = models.CharField(max_length=100, null=False)
-    lat = models.FloatField(('Latitude'), blank=True, null=True)
-    lon = models.FloatField(('Longitude'), blank=True, null=True)
-    analyser = models.ForeignKey(Analyser)
+
+    #lat is the latitude cordinate of the station
+	lat = models.FloatField(('Latitude'), blank=True, null=True)
+    
+	#lon is the longitude coordinate of the station
+	lon = models.FloatField(('Longitude'), blank=True, null=True)
+
+    #anaylser is the indentifier of analyser at a station
+	analyser = models.ForeignKey(Analyser)
 
     def __unicode__(self):
+		#returns station name
         return self.station_name
-
+		
+	#function get_absolute_url points to a particular station
     def get_absolute_url(self):
         from django.core.urlresolvers import reverse
         return reverse('station-detail', args=[self.id,])
--- hewa/tables.py
+++ hewa/tables.py
@@ -2,10 +2,12 @@ import django_tables2 as tables
 from hewa.models import Station
 
 class StationTable(tables.Table):
+	#capture station name
 	station_name = tables.Column(verbose_name="station_name")
-	# lat = tables.Column(verbose_name="latitude")
-	# lon = tables.Column(verbose_name="longitude")
+		
+	#capture anaylser id
 	analyser = tables.Column(verbose_name="analyser")
+	
 	selection = tables.CheckBoxColumn(accessor="pk", orderable=False)
 
 	class Meta:
