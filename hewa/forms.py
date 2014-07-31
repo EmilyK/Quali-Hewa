3398cf41e8b6b79c057f5b12d1b55ae9f742704c
Marceee <marcelinusande@yahoo.com>
2014-07-31 11:46:16 +0300

    Update forms.py

--- hewa/forms.py
+++ hewa/forms.py
@@ -3,13 +3,13 @@ from .lookups import StationLookup #lookups comes from selectable
 from .models import Station
 from selectable import forms as selectable_forms
 
-
+# validation for station form input
 class StationForm(forms.Form):
 	autocomplete = forms.CharField(label='Type the name of station',
 					widget=selectable_forms.AutoCompleteWidget(StationLookup),
 					required=False)
 
-
+#validation for the upload
 class UploadForm(forms.Form):
 	identifier = forms.CharField(required=True)
 	carbonmonoxide_sensor_reading = forms.DecimalField(required=True)
